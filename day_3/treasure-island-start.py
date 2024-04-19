66e3ffa70315e72a3037af658e36fc6ebf746920
@rti khot <61792772+artikhot97@users.noreply.github.com>
2024-04-19 16:41:17 +0530

    Create treasure-island-start.py

---
+++ day_3/treasure-island-start.py
@@ -0,0 +1,83 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+print("You are at a crossroads. To the left is a dark forest, to the right is a beautiful beach. Which way do you want to go?")
+direction = input("Type 'left' or 'right'.\n").lower()
+if direction == "left":
+  print("You enter the dark forest. You hear a rustling in the bushes. Do you investigate?")
+  investigate = input("Type 'yes' or 'no'.\n").lower()
+  if investigate == "yes":
+    print("You find a small creature. It looks like a bear. Do you try to pet it?")
+    pet = input("Type 'yes' or 'no'.\n").lower()
+    if pet == "yes":
+      print("The bear is very friendly and gives you a treasure map. You win!")
+    else:
+      print("The bear attacks you and you die. Game over.") 
+  else:
+    print("You continue on your journey. You come across a river. Do you swim across or wait for a boat?")
+    swim = input("Type 'swim' or 'wait'.\n").lower()
+    if swim == "swim":
+      print("You swim across the river and find a treasure chest. You win!")
+    else:
+      print("A boat arrives and takes you to the other side of the river. You find a treasure chest. You win!")
+
+
+#Output
+# *******************************************************************************
+#           |                   |                  |                     |
+#  _________|________________.=""_;=.______________|_____________________|_______
+# |                   |  ,-"_,=""     `"=.|                  |
+# |___________________|__"=._o`"-._        `"=.______________|___________________
+#           |                `"=._o`"=._      _`"=._                     |
+#  _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+# |                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+# |___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+#           |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+#  _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+# |                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+# |___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+# ____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+# /______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+# ____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+# /______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+# ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+# /______/______/______/______/______/______/______/______/______/______/_____ /
+# *******************************************************************************
+
+# Welcome to Treasure Island.
+# Your mission is to find the treasure.
+# You are at a crossroads. To the left is a dark forest, to the right is a beautiful beach. Which way do you want to go?
+# Type 'left' or 'right'.
+# left
+# You enter the dark forest. You hear a rustling in the bushes. Do you investigate?
+# Type 'yes' or 'no'.
+# no
+# You continue on your journey. You come across a river. Do you swim across or wait for a boat?
+# Type 'swim' or 'wait'.
+# swim
+# You swim across the river and find a treasure chest. You win!
